551e03f8b05d7b694de1d26b1887ff73d8094a65
Gabriele <gabri46nicotra@gmail.com>
2024-03-17 23:09:46 +0100

    adjusted bg-color

--- script/Info.js
+++ script/Info.js
@@ -8,3 +8,11 @@ onscroll = () => {
     }
 }
 const buttonTop = () => document.documentElement.scrollTop = 0;
+/* colors light mode:
+1st svg => bg-[#FAF9FA] fill-[#D8CBED]
+discover ecosystem => discover text-[#280D5F] eco=> text-colorsSecondary
+card => bg-white
+button buy cake text-white 
+2nd svg => bg-[#FAF9FA] fill-[#72B8F2]
+
+*/
